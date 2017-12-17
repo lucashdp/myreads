07e1c5317e3acd6cad14c1e02634ce6cb5846ca1
lucashdp <lucashdp@outlook.com>
2017-12-17 21:28:17 -0200

    refactor: componento book refactored

--- src/Book.js
+++ src/Book.js
@@ -2,51 +2,50 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ChangeShelf from './ChangeShelf'
 
-class SearchPage extends React.Component {
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onUpdateBook: PropTypes.func.isRequired
+const serializeAuthors = (authors = ['', '']) => {
+
+    if (authors.length === 1)
+        return authors
+
+    let serializedAuthors = '';
+
+    for (const author of authors) {
+        serializedAuthors += author + ', '
     }
 
-    serializeAuthors(authors = ['', '']) {
-
-        if (authors.length === 1)
-            return authors
-
-        let serializedAuthors = '';
-
-        for (const author of authors) {
-            serializedAuthors += author + ', '
-        }
-
-        return serializedAuthors.substring(0, serializedAuthors.length - 2) + '.';
-    }
-
-    render() {
-        const { book, onUpdateBook } = this.props
-
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover"
-                        style={{
-                            width: 128, height: 193,
-                            backgroundImage: `url(${book.imageLinks !== undefined ? 
-                             book.imageLinks.smallThumbnail : ''})`
-                        }}>
-                    </div>
-                    <div className="book-shelf-changer">
-                        <ChangeShelf
-                            book={book}
-                            onUpdateBook={() => onUpdateBook(book)}
-                        />
-                    </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{this.serializeAuthors(book.authors)}</div>
-            </div>
-        )
-    }
+    return serializedAuthors.substring(0, serializedAuthors.length - 2) + '.';
 }
 
-export default SearchPage
+const Book = (props) => {
+
+    const { book, onUpdateBook } = props
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover"
+                    style={{
+                        width: 128, height: 193,
+                        backgroundImage: `url(${book.imageLinks !== undefined ?
+                            book.imageLinks.smallThumbnail : ''})`
+                    }}>
+                </div>
+                <div className="book-shelf-changer">
+                    <ChangeShelf
+                        book={book}
+                        onUpdateBook={() => onUpdateBook(book)}
+                    />
+                </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{serializeAuthors(book.authors)}</div>
+        </div>
+    )
+}
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    onUpdateBook: PropTypes.func.isRequired
+}
+
+export default Book
